0a28300d6f9a87a6088fad7aa1e6e48fc8354c34
Jonathan Phipps <jonathanmp25@gmail.com>
2018-06-04 13:52:48 -0500

    Express configuration

---
+++ helpers/mongoUtil.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+const DB_URL = 'mongodb://localhost:27017';
+const DB_NAME = '';
+let db;
+
+module.exports = {
+
+  connectToServer(callback) {
+    MongoClient.connect(DB_URL, (err, client) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+        process.exit(1);
+      }
+      db = client.db(DB_NAME);
+      return callback(err);
+    });
+  },
+
+  getDb() {
+    return db;
+  },
+};
