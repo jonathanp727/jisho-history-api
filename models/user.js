e2d4cb817fe9f9bafe52197f55ff32999244695d
Jonathan Phipps <jonathanmp25@gmail.com>
2018-06-08 13:18:48 -0500

    Set up initial user model

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const ObjectID = require('mongodb').ObjectID;
+
+const mongoUtil = require('../helpers/mongoUtil.js');
+const cryptoUtil = require('../helpers/cryptoUtil.js');
+
+const collectionName = 'users';
+
+exports.all = (callback) => {
+  mongoUtil.getDb().collection(collectionName).find().toArray((err, result) => {
+    callback(err, result);
+  });
+};
+
+exports.get = (id, callback) => {
+  mongoUtil.getDb().collection(collectionName).findOne({ _id: ObjectID(id) }, (err, result) => {
+    callback(err, result);
+  });
+};
+
+exports.new = (data, callback) => {
+  const hashResult = cryptoUtil.saltHashPassword(data.password);
+  mongoUtil.getDb().collection(collectionName).insertOne({
+    data.username,
+    password: hashResult.passwordHash,
+    salt: hashResult.salt,
+    data.isAdmin,
+  }, (err, result) => {
+    callback(err, result);
+  });
+};
+
+exports.update = (id, data, callback) => {
+  const hashResult = cryptoUtil.saltHashPassword(data.password);
+  mongoUtil.getDb().collection(collectionName).updateOne({ _id: ObjectID(id) }, {
+    username: data.username,
+    password: hashResult.passwordHash,
+    salt: hashResult.salt,
+  }, (err) => {
+    callback(err);
+  });
+};
+
+exports.delete = (id, callback) => {
+  mongoUtil.getDb().collection(collectionName).deleteOne({ _id: ObjectID(id) }, (err) => {
+    callback(err);
+  });
+};
--- routes/join.js
+++ routes/join.js
@@ -6,7 +6,8 @@ const userModel = require('../models/user.js');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
-  userModel.new(req.body.username, req.body.password, false, (err, result) => {
+  req.body.isAdmin = false;
+  userModel.new(req.body, (err, result) => {
     if (err) return next(err);
     const token = jwt.sign(result, process.env.JWT_KEY);
     res.json({ success: true, message: 'Authenticated', token });
